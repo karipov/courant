e66057fb336bbf428c41cb92aae93345fb461772
Komron <komron_aripov@cdl.ch>
2019-06-06 09:23:48 +0200

    connect to mongodb

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,16 @@
+from json import load
+from pathlib import Path
+
+from mongoengine import connect
+
 from .channels import Channel
 from .rss import RSS
-from .users import User
+from .users import User, Settings
 
-__all__ = ['Channel', 'RSS', 'User']
+
+__all__ = ['Channel', 'RSS', 'User', 'Settings']
+
+# loading the configuration file
+args = load(open(Path.cwd().joinpath('src/config.json')))
+# connecting to a mongo database
+connect(args['MONGO']['name'])
