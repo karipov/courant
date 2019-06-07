5a0cabb5c79ff6d7704d7b01edba16d6975ec8db
Komron <komron_aripov@cdl.ch>
2019-06-07 10:10:02 +0200

    code refactoring: /start cmd

--- src/handlers/service.py
+++ src/handlers/service.py
@@ -6,17 +6,30 @@ import utility
 
 
 markup = utility.gen_keyboard(label=txt['LANG_NAMES'], data=txt['LANGS'])
-print(markup)
+
+
+def get_payload(text: str):
+    """
+    Convenience proxy function for utility.extract_payload
+
+    :param text: text containing the payload
+    :return: extracted payload
+    """
+    try:
+        return int(utility.extract_payload(text)[0])
+    except (TypeError, IndexError, ValueError):
+        return None
 
 
 def cmd_start(update, context):
+    """
+    Handler: command /start <PAYLOAD>
+    """
     uid = update.message.chat_id
-    lang = update.message.from_user.language_code
+    lang = utility.lang(update.message.from_user.language_code, txt['LANGS'])
+    payload = get_payload(update.message.text)
 
     print(f'uid {uid} - lang {lang} has been detected')
-
-    if lang not in txt['LANGS']:
-        lang = 'en'
 
     # build account for a user if it doesn't exist
     if not User.check_exists(uid=uid):
@@ -26,47 +39,19 @@ def cmd_start(update, context):
             settings=Settings(language=lang)
         ).save()
 
-    # extract and turn the payload into an integer
-    try:
-        print(f'message text: {update.message.text}')
-        payload = int(utility.extract_payload(update.message.text)[0])
-        print(f'payload is: {payload}')
-    except (TypeError, IndexError):
-        print('payload is empty')
-        payload = None
-
     if not payload:
         context.bot.send_message(
-            uid, txt['SERVICE']['start']['en'],
+            uid, txt['SERVICE']['start'][lang],
             reply_markup=markup
             )
         return
 
-    # update whoever invited the new user
-    # we also account for whether the inviter is
-    # already in the database or not
-    try:
-        inviter = User.objects.get(user_id=payload)
-        if uid in inviter.users_invited or uid == payload:
-            print("user already invited or is the inviter themselves")
-            # maybe provide an error message here?
-            # "you've already invited this user"
-            return
+    inviter = User.get_user(uid=payload)
 
-        print(f'inviter uid {inviter.user_id}')
-        print(f'inviter already invited: {inviter.users_invited}')
+    if not inviter:
+        return
 
-        # append & save our changes on the inviter
-        inviter.users_invited.append(uid)
-        inviter.save()
-
-        context.bot.send_message(
-            uid, txt['SERVICE']['invited_by']['en'].format(payload)
-        )
-
-        print(f'inviter now has invited: {inviter.users_invited}')
-    except Exception as e:
-        # catches if the user doesn't exist
-        print('panic', e)
-
-    print()
+    inviter.add_to_invited(uid)
+    context.bot.send_message(
+        uid, txt['SERVICE']['invited_by'][lang].format(payload)
+    )
