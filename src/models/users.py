e70471d3036212d6761114905c63e77d44c28e32
Komron <komron_aripov@cdl.ch>
2019-06-07 09:45:13 +0200

    utility methods and clsmethods

--- src/models/users.py
+++ src/models/users.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from mongoengine import Document
 from mongoengine import IntField, DateTimeField, ListField, StringField
 from mongoengine import BooleanField, EmbeddedDocument, EmbeddedDocumentField
@@ -20,6 +22,34 @@ class User(Document):
         'indexes': ['user_id'],
         'collection': 'users'
     }
+
+    def add_to_invited(self, payload: int):
+        """
+        :param received: user_id of the user to whom we are adding
+        :param payload: the user_id we are adding
+        """
+        # TODO: add a user_id to invited list
+        if payload in self.users_invited or self.user_id == payload:
+            return
+
+        self.users_invited.append(payload)
+        self.save()
+
+    @classmethod
+    def get_user(cls, uid: int) -> User:
+        """
+        Fetches a single user for a given user_id
+
+        :param uid: the user_id to fetch by
+        :return: User object
+        """
+        user = cls.objects(user_id=uid)
+
+        if len(user) == 0 or len(user) > 1:
+            return None
+
+        # return the single element form the list
+        return user[0]
 
     @classmethod
     def check_exists(cls, uid: int) -> bool:
