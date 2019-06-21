4ef25aff155fe573d949b59fabfc1e8a15e12625
Komron <komron_aripov@cdl.ch>
2019-06-22 00:57:56 +0200

    implement total users

--- src/handlers/admin.py
+++ src/handlers/admin.py
@@ -6,6 +6,7 @@ use of checking for admin priveleges, so a check function is included.
 from .shared import (
     txt, config
 )
+from models import User
 
 
 def check_admin(uid: int):
@@ -14,7 +15,7 @@ def check_admin(uid: int):
 
     :param uid: user_id to check for.
     """
-    if str(uid) == config['admin']:
+    if uid in config['TELEGRAM']['admin']:
         return True
     else:
         return False
@@ -22,9 +23,12 @@ def check_admin(uid: int):
 
 def cmd_admin(update, context):
     uid = update.message.chat_id
+
     if not check_admin(uid):
         return
 
+    total_users = User.retrieve_total()
+
     context.bot.send_message(
-        uid, txt['ADMIN']['stats']['en']
+        uid, txt['ADMIN']['stats']['en'].format(total_users)
     )
