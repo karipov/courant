c6cdb4259b2316dc39f9f94d78a9da132de18376
Komron <komron_aripov@cdl.ch>
2019-06-22 21:47:40 +0200

    implemented CallbackQuery handler in main; note: next time commit both together

--- src/main.py
+++ src/main.py
@@ -2,9 +2,13 @@ from pathlib import Path
 from json import load
 import logging
 
-from handlers import admin, service
+from handlers import (
+    admin, service, callback
+)
 
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import (
+    Updater, CommandHandler, CallbackQueryHandler
+)
 
 
 # set some config files and define them as objects
@@ -13,9 +17,14 @@ updater = Updater(token=args['TELEGRAM']['token'], use_context=True)
 dispatcher = updater.dispatcher
 
 
-# add stuff to the dispatcher
+# ADDING HANDLERS HERE
+# add command handlers
 dispatcher.add_handler(CommandHandler('admin', admin.cmd_admin))
 dispatcher.add_handler(CommandHandler('start', service.cmd_start))
+dispatcher.add_handler(CommandHandler('help', service.cmd_help))
+
+# add callback handlers
+dispatcher.add_handler(CallbackQueryHandler(callback.cmd_language_select))
 
 # start polling
 logging.basicConfig(level=logging.INFO)
