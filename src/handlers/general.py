00068abe7c9ac7120375799cac2255457ea64e28
Komron <komron_aripov@cdl.ch>
2019-06-23 20:29:24 +0200

    new file for general functionality

---
+++ src/handlers/general.py
@@ -0,0 +1,20 @@
+"""
+Handles "general" functionality that pertains to the workings of the program
+post-setup.
+
+Currently contains handlers for:
+- text-messages within the set-up FSM states
+
+Currently contains filters for:
+- text messages with certain FSM states
+"""
+
+# from .shared import (
+#     txt, FSM
+# )
+# from models import User
+
+
+def unprompted_message_handler(update, context):
+    """ Handler for unprompted messages """
+    pass
