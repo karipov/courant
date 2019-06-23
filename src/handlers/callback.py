0f85419601a5bea16c378ad1a2ad8a5e84b5618f
Komron <komron_aripov@cdl.ch>
2019-06-23 20:29:57 +0200

    callback now contains keyboard and text generation

--- src/handlers/callback.py
+++ src/handlers/callback.py
@@ -1,8 +1,8 @@
 from .shared import (
-    txt
+    txt, FSM
 )
-
 from models import User
+import utility
 
 
 def cmd_language_select(update, context):
@@ -12,9 +12,22 @@ def cmd_language_select(update, context):
     chosen_language_code = query.data
 
     user.settings.language = chosen_language_code
+    user.settings.fsm_state = FSM.MANUAL_ENTRY.value
     user.save()
+
+    new_content = (
+        f"{txt['CALLBACK']['lang_select'][chosen_language_code]}\n"
+        f"\n\n"
+        f"{txt['FSM'][FSM.MANUAL_ENTRY.value]['text'][chosen_language_code]}"
+    )
+
+    keyboard = utility.gen_keyboard(
+        txt['FSM'][FSM.MANUAL_ENTRY.value]['markup'][chosen_language_code],
+        txt['FSM'][FSM.MANUAL_ENTRY.value]['payload']
+    )
 
     # edit the message to display the selected language
     query.edit_message_text(
-        text=txt['CALLBACK']['lang_select'][chosen_language_code]
+        text=new_content,
+        markup=keyboard
     )
