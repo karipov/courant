37d405d8ac69b832c9f8524c676ba4a821ea27e2
Komron <komron_aripov@cdl.ch>
2019-06-26 23:26:54 +0200

    scraping module: initial commit

---
+++ src/scrape/__init__.py
@@ -0,0 +1,7 @@
+"""
+Meant for code that pertains to scraping of information or other data from the
+news sources each user has selected. This will include:
+
+- parsers for RSS/Atom links
+- userbot accounts for Telegram Channels
+"""
