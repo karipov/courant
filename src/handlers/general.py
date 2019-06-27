2c5966223538156c0772f2ff3d728b2a86f1a4ad
Komron <komron_aripov@cdl.ch>
2019-06-28 01:00:08 +0200

    drawing out handlers for general user-sent text messages.

--- src/handlers/general.py
+++ src/handlers/general.py
@@ -18,11 +18,39 @@ def master(update, context):
     messages.
     """
     db_user = User.get_user(update.message.from_user.id)
-    state = db_user.settings.fsm_state
+    user_state = db_user.settings.fsm_state
 
     # TODO: fill the fsm_options
     fsm_options = {
-        '': ''
+        '2.1.1': rss_compile,
+        '2.1.2': channel_compile,
+
+        '2.2.1': explore_compile
     }
 
-    fsm_options[state](update, context, db_user)
+    fsm_options[user_state](update, context, db_user)
+
+
+def general_compile(update, context, user):
+    """
+    Handles general messages for compilation.
+
+    This could be useful in case the need arises to add more services,
+    which will all basically use the same backend or same pieces of code.
+    """
+    pass
+
+
+def rss_compile(update, context, user):
+    """ Handler: fsm:2.1.1 -> fsm:3 """
+    pass
+
+
+def channel_compile(update, context, user):
+    """ Handler: fsm:2.1.2 -> fsm:3 """
+    pass
+
+
+def explore_compile(update, context, user):
+    """ Handler: fsm:2.2.1 -> fsm:3 """
+    pass
