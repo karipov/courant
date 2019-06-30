c39934a9b8a488af5b9590c5e3938807bfc943e3
Komron <komron_aripov@cdl.ch>
2019-06-30 22:49:01 +0200

    new rss validation and fields

--- src/models/rss.py
+++ src/models/rss.py
@@ -1,13 +1,27 @@
 from mongoengine import Document
 from mongoengine import StringField, IntField, ListField, DateTimeField
-import datetime
+from datetime import datetime
 
 
 class RSS(Document):
     time_added = DateTimeField(default=datetime.datetime.utcnow)
+
     link = StringField(required=True)
+    title = StringField(required=True)
+    subtitle = StringField(required=True)
+    last_published = DateTimeField(required=True, default=datetime.utcnow)
     subscribed = ListField(IntField(), required=True)
 
+    def clean(self):
+        """
+        Additional validation for RSS.
+        """
+        if len(self.title) > 255:
+            self.title = ''.join(self.title.split()[:255])
+        if len(self.subtitle) > 255:
+            self.subtitle = ''.join(self.subtitle.split()[:255])
+
     meta = {
-        'collection': 'rss'
+        'collection': 'rss',
+        'indexes': ['$title']
     }
