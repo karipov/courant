c5add6ffc31b0955f83c9fcf4f70bbd86f32405e
Komron <komron_aripov@cdl.ch>
2019-07-04 22:58:34 +0200

    handle RSS feed sending

--- src/handlers/general.py
+++ src/handlers/general.py
@@ -8,8 +8,11 @@ Currently contains handlers for:
 Currently contains filters for:
 - text messages with certain FSM states
 """
+from .shared import txt, config
+from models import User, RSS
+from scrape import rss_parse
 
-from models import User
+from mongoengine import errors
 
 
 def master(update, context):
@@ -20,12 +23,11 @@ def master(update, context):
     db_user = User.get_user(update.message.from_user.id)
     user_state = db_user.settings.fsm_state
 
-    # TODO: fill the fsm_options
     fsm_options = {
         '2.1.1': rss_compile,
         '2.1.2': channel_compile,
 
-        '2.2.1': explore_compile
+        '2.2': explore_compile
     }
 
     fsm_options[user_state](update, context, db_user)
@@ -42,8 +44,94 @@ def general_compile(update, context, user):
 
 
 def rss_compile(update, context, user):
-    """ Handler: fsm:2.1.1 -> fsm:3 """
-    pass
+    """ Handler: fsm:2.1.1 -> ___ """
+    # TODO: /done command should finish the compilation
+    text = update.message.text
+    news = rss_parse.parse_url(text)
+
+    # check the source for possible errors, such as bozo and format
+    if not rss_parse.check_source(news):
+        context.bot.send_message(
+            chat_id=user.user_id,
+            text=txt['CALLBACK']['error_link'][user.settings.language]
+        )
+        return
+
+    # check the actual feed, i.e.:
+    # stuff like title, subtitle, link and such.
+    checked_feed = rss_parse.check_parsed(
+        news.feed, config['SCRAPE']['RSS']['req_feed_keys']
+    )
+
+    # implement the checking described above
+    if not checked_feed:
+        context.bot.send_message(
+            chat_id=user.user_id,
+            text=txt['CALLBACK']['error_feed'][user.settings.language]
+        )
+        return
+
+    # now, all entries must be checked for certain required elements
+    # that will hopefully enrich user experience.
+    # this must strike a fine balance between universality and enough
+    # information for a good display of the RSS feed..
+    checked_all_entries = all([
+        rss_parse.check_parsed(
+            x, config['SCRAPE']['RSS']['req_entry_keys']
+            ) for x in news.entries
+        ])
+
+    # implement the checking above
+    if not checked_all_entries:
+        context.bot.send_message(
+            chat_id=user.user_id,
+            text=txt['CALLBACK']['error_entries'][user.settings.language]
+        )
+        return
+
+    # if all the checks have so far been passed, then we create the RSS
+    # feed in our database and register it - unless it already exists.
+
+    try:
+        db_news = RSS(
+            link=news.feed.link,
+            title=news.feed.title,
+            subtitle=news.feed.get('subtitle', ''),
+            summary=news.feed.get('summary', ''),
+        )
+        db_news.subscribed.append(user.user_id)
+        db_news.save()
+    except errors.NotUniqueError:
+        # TODO: process the error by sending a message
+        return
+
+    user.subscribed.rss_list.append(db_news.pk)
+    user.save()
+
+    feed_formatted = f"<a href=\"{db_news.link}\">{db_news.title}</a>"
+
+    if not len(news.entries) > 0:
+        context.bot.send_message(
+            chat_id=user.user_id,
+            text=feed_formatted +
+            txt['CALLBACK']['empty_feed'][user.settings.language],
+            parse_mode='HTML'
+        )
+        return
+
+    db_news.last_entry_link = news.entries[0].link
+    db_news.save()
+
+    context.bot.send_message(
+        chat_id=user.user_id,
+        text=feed_formatted +
+        txt['CALLBACK']['rss_success'][user.settings.language],
+        parse_mode='HTML'
+    )
+
+    # TODO: add "Send <smth> when done"
+    # TODO: remove RSS feed from list if it has no subscribers
+    # TODO: implement RSS feed removal
 
 
 def channel_compile(update, context, user):
@@ -52,5 +140,4 @@ def channel_compile(update, context, user):
 
 
 def explore_compile(update, context, user):
-    """ Handler: fsm:2.2.1 -> fsm:3 """
     pass
