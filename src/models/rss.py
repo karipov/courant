463c5c6dd004099f63b58ac0503cf5fcd57941bd
Komron <komron_aripov@cdl.ch>
2019-07-04 22:58:58 +0200

    new fields for richer UX

--- src/models/rss.py
+++ src/models/rss.py
@@ -4,22 +4,16 @@ from datetime import datetime
 
 
 class RSS(Document):
-    time_added = DateTimeField(default=datetime.datetime.utcnow)
+    time_added = DateTimeField(default=datetime.utcnow)
 
-    link = StringField(required=True)
+    link = StringField(unique=True, required=True)
     title = StringField(required=True)
-    subtitle = StringField(required=True)
-    last_published = DateTimeField(required=True, default=datetime.utcnow)
-    subscribed = ListField(IntField(), required=True)
 
-    def clean(self):
-        """
-        Additional validation for RSS.
-        """
-        if len(self.title) > 255:
-            self.title = ''.join(self.title.split()[:255])
-        if len(self.subtitle) > 255:
-            self.subtitle = ''.join(self.subtitle.split()[:255])
+    subtitle = StringField(default=str)
+    summary = StringField(default=str)
+    last_entry_link = StringField(required=True, default=str)
+
+    subscribed = ListField(IntField(), required=True, default=list)
 
     meta = {
         'collection': 'rss',
