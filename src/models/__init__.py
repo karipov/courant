1fab239b1518e0cffa7520b3a762effdc21fe313
Komron <komron_aripov@cdl.ch>
2019-10-09 14:18:22 +0200

    clear db at start of evers test

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -12,4 +12,8 @@ __all__ = ['Channel', 'RSS', 'User', 'Settings']
 # loading the configuration file
 args = load(open(Path.cwd().joinpath('src/config.json')))
 # connecting to a mongo database
-connect(args['MONGO']['name'])
+db = connect(args['MONGO']['name'])
+
+# clears database before start of every test.
+if args['MONGO']['dev_mode']:
+    db.drop_database(args['MONGO']['name'])
