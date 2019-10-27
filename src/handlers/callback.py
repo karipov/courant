45c8c804f00654cd721cbd11bfa9d227bc0a06cb
Komron <komron_aripov@cdl.ch>
2019-10-27 18:40:32 +0100

    channels can now also be deleted from menu

--- src/handlers/callback.py
+++ src/handlers/callback.py
@@ -2,8 +2,6 @@ from . import txt, config, remove_message
 from models import User
 import utility
 
-import logging
-
 
 def master_callback(update, context):
     """
@@ -25,10 +23,6 @@ def master_callback(update, context):
         tree = txt['FSM']['TREE']
     elif data_filter[0] == config['CB_DATA']['post_setup']:
         tree = txt['FSM']['DONE_TREE']
-    # elif data_filter[0] == config['CB_DATA']['operations']:
-    #     # has nothing to do with FSM
-    #     operations_callback(update, context, db_user)
-    #     return
     else:
         remove_message(update, context, db_user)
         return
@@ -38,9 +32,6 @@ def master_callback(update, context):
         future=data_filter[1],
         tree=tree
         )
-
-    logging.debug(f"current user state (db - exec.): {user_state}")
-    logging.debug(f"future user state (json): {data_filter[1]}")
 
     if not checked:
         remove_message(update, context, db_user)
@@ -59,7 +50,8 @@ def master_callback(update, context):
         '3.1': modify_rss_callback,
         '3.1.1': delete_rss_callback,
 
-        '3.2': general_callback,
+        '3.2': modify_channels_callback,
+        '3.2.1': delete_channel_callback,
 
         '3.3': general_callback,
         '3.3.1': general_callback
@@ -102,7 +94,7 @@ def general_callback(update, context, user):
 
 
 def cmd_entry_type(update, context, user):
-    """ Handler: fsm:1 -> fsm:2 """
+    """ Handler: 1 -> 2 """
     # custom handler needed to ensure language set-up
     query = update.callback_query
     chosen_language_code = query.data.split(config['TELEGRAM']['delim'])[2]
@@ -114,22 +106,22 @@ def cmd_entry_type(update, context, user):
 
 
 def manual_explore_entry(update, context, user):
-    """ Handler fsm:2 -> fsm:2.1 | fsm:2.2"""
+    """ Handler 2 -> 2.1 | 2.2"""
     general_callback(update, context, user)
 
 
 def modify_rss_callback(update, context, user):
-    """ Handler fsm:3.1 -> fsm:3 | fsm:3.1.1"""
+    """ Handler 3.1 -> 3 | 3.1.1"""
     query = update.callback_query
     future_state = query.data.split(config['CB_DATA']['delim'])[1]
 
-    if future_state == "3":
+    if future_state == '3':
         general_callback(update, context, user)
         return
 
     # TODO: I beliebe telegram allows only a certain number of
     # items as buttons... Maybe having more should be a #premium feature
-    inline = [[item.title, f"fin:3.1.1:{i}"] for i, item in enumerate(
+    inline = [[item.title, f'fin:3.1.1:{i}'] for i, item in enumerate(
         user.subscribed.rss_list
     )]
     # adding the "Back" button. since only one button we use index 0
@@ -151,12 +143,12 @@ def modify_rss_callback(update, context, user):
         parse_mode='HTML'
     )
 
-    user.settings.fsm_state = "3.1.1"
+    user.settings.fsm_state = future_state
     user.save()
 
 
 def delete_rss_callback(update, context, user):
-    """ Handler fsm:3.1.1 -> Delete RSS | 3.1 """
+    """ Handler 3.1.1 -> Delete RSS | 3.1 """
     query = update.callback_query
     future_state = query.data.split(config['CB_DATA']['delim'])[1]
 
@@ -190,3 +182,76 @@ def delete_rss_callback(update, context, user):
     user.save()
 
     modify_rss_callback(update, context, user)
+
+
+def modify_channels_callback(update, context, user):
+    """ Handler 3.2 -> 3 | 3.2.1 """
+    # TODO: this is the same as modify_rss_callback
+    # except the future_state const in 'inline' definition
+    query = update.callback_query
+    future_state = query.data.split(config['CB_DATA']['delim'])[1]
+
+    if future_state == '3':
+        general_callback(update, context, user)
+        return
+
+    inline = [[item.title, f'fin:3.2.1:{i}'] for i, item in enumerate(
+        user.subscribed.channel_list
+    )]
+    inline.append([
+        txt['FSM'][future_state]['markup'][user.settings.language][0],
+        txt['FSM'][future_state]['payload'][0]
+    ])
+    label, data = zip(*inline)
+
+    keyboard = utility.gen_keyboard(
+        label=label,
+        data=data,
+        width=2
+    )
+
+    query.edit_message_text(
+        text=txt['FSM'][future_state]['text'][user.settings.language],
+        reply_markup=keyboard,
+        parse_mode='HTML'
+    )
+
+    user.settings.fsm_state = future_state
+    user.save()
+
+
+def delete_channel_callback(update, context, user):
+    """ Handler 3.2.1 -> Delete Channel | 3.2 """
+    query = update.callback_query
+    future_state = query.data.split(config['CB_DATA']['delim'])[1]
+
+    if future_state == "3.2":
+        general_callback(update, context, user)
+        return
+
+    # TODO: error check here.
+    resource_delete_id = int(query.data.split(config['CB_DATA']['delim'])[-1])
+    channel_resource = user.subscribed.channel_list[resource_delete_id]
+
+    # if the RSS feed has only one subscribed, then we disable fetching feed
+    if len(channel_resource.subscribed) == 1:
+        channel_resource.fetched = False
+        channel_resource.subscribed = []
+    else:
+        channel_resource.subscribed.remove(user.user_id)
+
+    channel_resource.save()
+
+    context.bot.answer_callback_query(
+        callback_query_id=query.id,
+        text=txt['CALLBACK']['deleted_channel'][user.settings.language].format(
+            user.subscribed.channel_list[resource_delete_id].title
+        ),
+        alert=True
+    )
+
+    # deletes the Channel resource from User's document
+    del user.subscribed.channel_list[resource_delete_id]
+    user.save()
+
+    modify_channels_callback(update, context, user)
