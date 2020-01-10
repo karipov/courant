430abc5e42073804a685a0ef0eca064d32126a7e
Komron <komron_aripov@cdl.ch>
2020-01-10 22:57:09 +0100

    initilialize client inside the loop

--- src/scrape/__init__.py
+++ src/scrape/__init__.py
@@ -15,37 +15,37 @@ class Scraper:
         :param config: dict object containing config data.
         :param bot: python-telegram-bot object
         """
-        self.client = Client(
-            session_name=config['PYROGRAM']['update_session_file'],
-            workdir=Path.cwd().joinpath(
-                config['PYROGRAM']['sessions_path']
-                ),
-            api_id=config['PYROGRAM']['api_id'],
-            api_hash=config['PYROGRAM']['api_hash']
-        )
         self.bot = bot
         self.config = config
         self.txt = json.load(
             open(Path.cwd().joinpath('src/interface/replies.json'))
         )
 
-        # must not forget to stop the client.
-        self.client.start()
-
     # import functions from other modules
     from ._channel_scrape import update_channels, _get_new_posts, \
         _filter_post, _send_post
     from ._rss_scrape import update_rss_feeds, _full_feed_check, \
-        _get_new_entries
+        _get_new_entries, _send_rss_message
 
     def run(self):
         """
         Launch & run loop in a separate thread with updates.
         """
         def _loop_run():
+            # initializing client inside thread to avoid sqlite session errs
+            self.client = Client(
+                session_name=self.config['PYROGRAM']['update_session_file'],
+                workdir=Path.cwd().joinpath(
+                    self.config['PYROGRAM']['sessions_path']
+                    ),
+                api_id=self.config['PYROGRAM']['api_id'],
+                api_hash=self.config['PYROGRAM']['api_hash']
+            )
+            self.client.start()
+
             while True:
                 self.update_channels()
                 self.update_rss_feeds()
-                time.sleep(300)  # avoid floods
+                time.sleep(180)  # avoid floods
 
         Thread(target=_loop_run).start()
